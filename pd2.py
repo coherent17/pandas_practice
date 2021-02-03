694ffa5df6b2ae9aab0afc62b9510d5ac87a7034
coherent17 <mnb51817@gmail.com>
2021-02-04 00:40:22 +0800

    test

--- pd2.py
+++ pd2.py
@@ -8,3 +8,13 @@ print(a)
 #1   6   7   8   9  10
 #2  11  12  13  14  15
 #直的是index,橫的是column
+
+#setting index and column
+b=pd.DataFrame(a.values,index=['a','b','c'],columns=['A','B','C','D','E'])
+print(b)
+#    A   B   C   D   E
+#a   1   2   3   4   5
+#b   6   7   8   9  10
+#c  11  12  13  14  15
+
+#2. using dict to build the dataframe
