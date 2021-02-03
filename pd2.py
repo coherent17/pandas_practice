4226f1a0e10fb73086ce188457fe199f260f490a
coherent17 <mnb51817@gmail.com>
2021-02-04 00:26:37 +0800

    test

---
+++ pd2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+#1. build the dataframe
+a=pd.DataFrame([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]])
+print(a)
+#    0   1   2   3   4
+#0   1   2   3   4   5
+#1   6   7   8   9  10
+#2  11  12  13  14  15
