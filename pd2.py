6a337dc986cca3bab3db02b58e2a5e4525908459
coherent17 <mnb51817@gmail.com>
2021-02-04 21:50:24 +0800

    test

--- pd2.py
+++ pd2.py
@@ -18,3 +18,25 @@ print(b)
 #c  11  12  13  14  15
 
 #2. using dict to build the dataframe
+data={'A':{'a':1,'b':6,'c':11},
+      'B':{'a':2,'b':7,'c':12},                         
+      'C':{'a':3,'b':8,'c':13},  
+      'D':{'a':4,'b':9,'c':14},                        
+      'E':{'a':5,'b':10,'c':15}}
+c=pd.DataFrame(data)
+print(c)
+
+#3. using series to build the dataframe
+se1=pd.Series({'a':1,'b':6,'c':11})
+se2=pd.Series({'a':2,'b':7,'c':12})
+se3=pd.Series({'a':3,'b':8,'c':13})
+se4=pd.Series({'a':4,'b':9,'c':14})
+se5=pd.Series({'a':5,'b':10,'c':15})
+d=pd.DataFrame({'A':se1,'B':se2,'C':se3,'D':se4,'E':se5})
+print(d)
+
+#4.using concat function
+e=pd.concat([se1,se2,se3,se4,se5],axis=1)
+e.columns=['A','B','C','D','E']
+print(e)
+
